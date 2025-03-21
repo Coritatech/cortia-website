072e2bd17ace6310887bb08272e5dc4d7b604073
Tescodepro <tescodepro@gmail.com>
2025-03-22 00:28:22 +0100

    update

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,11 +1,28 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php echo $name; ?> &#8211; Cortia</title>
-    <meta name="description" content="">
+    <title>Cortia - <?php echo $name; ?></title>
+    
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Cortia Technologies - Leading in innovative tech solutions. provides top-notch services.">
+    <meta name="keywords" content="Cortia Technologies, Tech Solutions, , IT Services, Software Development">
+    <meta name="author" content="Cortia Technologies">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Canonical URL -->
+    <link rel="canonical" href="https://cortia.com.ng/">
 
-    <!-- Place favicon.ico in the root directory -->
+    <!-- Open Graph (Facebook, LinkedIn) -->
+    <meta property="og:title" content="Cortia - <?php echo $name; ?>">
+    <meta property="og:description" content="Cortia Technologies offers innovative tech solutions including .">
+    <meta property="og:image" content="https://cortia.com.ng/views/assets/img/logo/og-image.png">
+    <meta property="og:url" content="https://cortia.com.ng/">
+    <meta property="og:type" content="website">
+    
+    <!-- Robots Meta Tag -->
+    <meta name="robots" content="index, follow">
+    
+    <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="views/assets/img/logo/favicon.png">
 
     <!-- CSS here -->
@@ -18,4 +35,21 @@
     <link rel="stylesheet" href="views/assets/css/flaticon.css">
     <link rel="stylesheet" href="views/assets/css/spacing.css">
     <link rel="stylesheet" href="views/assets/css/main.css">
+
+    <!-- Structured Data (JSON-LD) -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Cortia Technologies",
+        "url": "https://cortia.com.ng",
+        "logo": "https://cortia.com.ng/views/assets/img/logo/logo.png",
+        "description": "Cortia Technologies offers innovative tech solutions including <?php echo $name; ?>.",
+        "sameAs": [
+            "https://www.facebook.com/cortiatechnologies",
+            "https://twitter.com/cortiatech",
+            "https://www.linkedin.com/company/cortiatechnologies"
+        ]
+    }
+    </script>
 </head>
